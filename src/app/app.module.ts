f4ba8a15ee390779e71c52eca5c51f0022fbe5c0
Ishwor Khadka <eshworkhadka@hotmail.com>
2021-11-28 22:06:59 +1030

    first commit to github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HouseComponent } from './house/house.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'dashboard', component: HeaderComponent,
+    children: [
+      {path: 'home', component: HomeComponent},
+      {path: 'about', component: AboutComponent},
+    ]
+  },
+  {
+    path: 'login', component:  LoginComponent
+  },
+  {
+    path: '**', redirectTo: '/login', pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    HouseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
